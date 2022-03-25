c2ab31838f699d0eb919273351108fb74c56c0ed
Nikhilrawal4775 <nikhilrawal4775@gmail.com>
2022-03-25 12:30:55 +0530

    latest

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return "Welcome to Home Page"
+
+@app.route("/nikhil")
+def nikhil():
+    return "Welcome nikhil"
     
 if __name__=='__main__':
     app.run(debug=True)
