481c9cb88d1d204074cd5b2da309a62a63a58d25
Nikhilrawal4775 <nikhilrawal4775@gmail.com>
2022-03-31 18:00:53 +0530

    latest

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "default" {
 
 resource "google_container_cluster" "primary" {
   name     = "nikhil-public-cluster"
-  location = "us-central1"
+  location = "us-central1-c"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
